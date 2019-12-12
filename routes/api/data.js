f89518393c14d296bda78c8cf8eee874451ceb10
acoltelli <acoltelli@protonmail.com>
2019-12-12 15:52:02 -0800

    Update data.js

--- routes/api/data.js
+++ routes/api/data.js
@@ -1,25 +1,25 @@
 const express = require("express");
 const router = express.Router();
-const Habit = require("../../models/Habit");
-const Validator = require("validator");
+const Test = require("../../models/Test");
+// const Validator = require("validator");
 const isEmpty = require("is-empty");
 
-// 
-// router.get(
-//   "/",
-//   async (req, res) => {
-//     const OWNER = {
-//       id: req.user.id,
-//       name: req.user.name,
-//       email: req.user.email
-//     };
-//     await Habit.find({ owner: OWNER })
-//       .then(habits => {
-//         res.json(habits);
-//       })
-//       .catch(err => console.log(err));
-//   }
-// );
+
+router.get(
+  "/",
+  async (req, res) => {
+    const OWNER = {
+      id: req.user.id,
+      name: req.user.name,
+      email: req.user.email
+    };
+    await Habit.find({ owner: OWNER })
+      .then(habits => {
+        res.json(habits);
+      })
+      .catch(err => console.log(err));
+  }
+);
 
 
 module.exports = router;
